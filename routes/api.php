19982d73d53b97e6650f48d3fc728899a6564b9e
David Hoang <88292464+David-Hoang@users.noreply.github.com>
2025-04-23 03:21:40 +0200

    Add middleware craftsman role + controller

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,14 @@
 <?php
 
+use App\Enums\Role;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Middleware\IsCraftsmanMiddleware;
 use App\Http\Controllers\Api\Auth\AuthController;
-use App\Http\Controllers\Api\CraftsmanJobController;
 use App\Http\Controllers\Api\Enum\EnumController;
+use App\Http\Controllers\Api\CraftsmanJobController;
+use App\Http\Controllers\Api\CraftsManJob\CraftsmanController;
+
 
 
 // Route::get('/user', function (Request $request) {
@@ -12,7 +16,8 @@ use App\Http\Controllers\Api\Enum\EnumController;
 // })->middleware('auth:sanctum');
 
 // Route::get('/hello', function() {
-//     return 'Hello world';
+//     dd(Role::CRAFTSMAN);
+//     // return Role::CRAFTSMAN;
 // });
 
 
@@ -21,15 +26,27 @@ Route::post('/login', [AuthController::class, 'login']);
 
 Route::get('/enums/regions', [EnumController::class, 'regions']);
 
+//Jobs categories
 Route::get('/jobs', [CraftsmanJobController::class, 'jobs']);
 Route::get('/jobs/{id}', [CraftsmanJobController::class, 'singleJob']);
 Route::post('/jobs/new-job', [CraftsmanJobController::class, 'addJob']);
+
+
 
 Route::middleware('auth:sanctum')->group(function() {
 
     Route::post('/auth', [AuthController::class, 'checkAuth']);
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::get('/all-users', [AuthController::class, 'allUsers']);
-    
+
+    Route::get('/hi', function() {
+    dd('aaa');
+    // return Role::CRAFTSMAN;
+    });
+
+    Route::middleware(IsCraftsmanMiddleware::class.':'.Role::CRAFTSMAN->value)->group(function() {
+        //Craftsman infos
+        Route::get('/craftsman-infos', [CraftsmanController::class, 'craftsmanInfos']);
+    });
 
 });
